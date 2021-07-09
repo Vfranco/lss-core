8f908ea45c3013d7d91e314a5ee8d350fc34bca2
victor-franco <vfrancov25@gmail.com>
2021-07-09 14:08:28 -0500

    LSS-CORE - adding directives

--- projects/lss-lib/src/lib/lss-sidebar/lss-sidebar.componet.ts
+++ projects/lss-lib/src/lib/lss-sidebar/lss-sidebar.componet.ts
@@ -3,6 +3,9 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'lss-sidebar',
   templateUrl: './lss-sidebar.component.html',
-  styleUrls: ['./lss-sidebar.component.scss']
+  styleUrls: ['./lss-sidebar.component.scss'],
+  host : {
+    class : 'lss-sidebar'
+  }
 })
 export class LssSidebarComponent { }
